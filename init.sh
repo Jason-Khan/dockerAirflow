30192369c6e8224d033d127c81053e35da0effc7
Jason-Khan <angean@qq.com>
2018-03-13 16:00:25 -0700

    try background services

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@ echo "----------------------Initiating database---------------------------------
 airflow initdb
 
 echo "----------------------Starting scheduler-----------------------------------"
-airflow scheduler
+airflow scheduler -D
 
 echo "----------------------Creating webserver on port 8080----------------------"
-airflow webserver -p 8080
+airflow webserver -p 8080 -D
 
 
