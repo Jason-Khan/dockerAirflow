ef50984583122a820c1faccb5551da707b13bae8
Jason-Khan <angean@qq.com>
2018-03-13 15:49:27 -0700

    airflow scheduler first

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # dockerAirflow
 ##### *implementing https://stlong0521.github.io/20161023%20-%20Airflow.html*
 ### (NOW) Stage 1: Standalone Mode Using Sequential Executor
+	build the base image as dockerbase first, then:
        Build: docker build -t jasonkhan/dockerairflow .
        Run: docker run -d -p 8080:8080 jasonkhan/dockerairflow
        *airflow webserver is exposed on port 8080.*
--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 echo "----------------------Initiating database----------------------------------"
 airflow initdb
 
+echo "----------------------Starting scheduler-----------------------------------"
+airflow scheduler
+
 echo "----------------------Creating webserver on port 8080----------------------"
 airflow webserver -p 8080
 
-echo "----------------------Starting scheduler-----------------------------------"
-airflow scheduler
 
