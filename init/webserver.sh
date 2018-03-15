51c143441d1709defad929b17adb36ae24a77f2f
Jason-Khan <angean@qq.com>
2018-03-15 15:11:19 -0700

    switch back to single_container

--- init/webserver.sh
+++ init/webserver.sh
@@ -2,4 +2,7 @@ echo "----------------------Initiating database---------------------------------
 airflow initdb
 
 echo "----------------------Creating webserver on port 8080----------------------"
-airflow webserver -p 8080
+airflow webserver -p 8080 -D
+
+echo "----------------------Creating scheduler----------------------"
+airflow scheduler -D
