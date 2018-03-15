8687d5cf39abb99909233ec5db311bd32a3cb647
Jason-Khan <angean@qq.com>
2018-03-15 15:27:21 -0700

    back to double container

--- init/webserver.sh
+++ init/webserver.sh
@@ -2,7 +2,4 @@ echo "----------------------Initiating database---------------------------------
 airflow initdb
 
 echo "----------------------Creating webserver on port 8080----------------------"
-airflow webserver -p 8080 -D
-
-echo "----------------------Creating scheduler----------------------"
-airflow scheduler -D
+airflow webserver -p 8080
